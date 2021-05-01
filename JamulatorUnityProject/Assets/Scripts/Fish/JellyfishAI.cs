ee86408b7b67ad37b7fe62c5a629a13425892c33
Matt Newbigging <mnewbigging@opus2.com>
2021-05-01 16:07:11 +0100

    improved fish ai performance

---
+++ JamulatorUnityProject/Assets/Scripts/Fish/JellyfishAI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyfishAI : BaseFishAI
+{
+    private Vector3 startPos;
+
+    private void Awake() {
+        startPos = transform.position;
+    }
+
+    
+        
+    
+}
