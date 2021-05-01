6107f911de31f2a62972e23109e12aba9dbe545f
xvelastin <80051955+xvelastin@users.noreply.github.com>
2021-05-01 12:09:27 +0100

    Added collision process, added mesh collider to GiantReef
    Replaced CollisionToAudio with SubmarineCollisionListener, because it was neater to process everything there and then send it off to the singletones in one place.

--- JamulatorUnityProject/Assets/Scripts/Audio/RTPC and Game Calls/PlayerSub/CollisionToAudio.cs
+++ JamulatorUnityProject/Assets/Scripts/Audio/RTPC and Game Calls/PlayerSub/CollisionToAudio.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionToAudio : MonoBehaviour
-{    
-    // TODO: need to work out how/whether it can be routed throught the event system //
+{
+    private void Start()
+    {
+        EventManager.Instance.onSubCollision += ProcessCollision;
+    }
 
-    void OnCollisionEnter(Collision collision)
+
+    void ProcessCollision(Collision collision)
     {
         float impactMagnitude = collision.relativeVelocity.magnitude;
         Vector3 position = collision.contacts[0].point;
-        AudioManager.Instance.PlayCollisionSound(position, impactMagnitude);
 
         Debug.Log("Collision! at position: " + position + " with an impactMagnitude of " + impactMagnitude);
 
-        EventManager.Instance.NotifyOfSubCollision(collision);
+        AudioManager.Instance.PlayCollisionSound(position, impactMagnitude);
+
+    }
+
+    private void OnDisable()
+    {
+        EventManager.Instance.onSubCollision -= ProcessCollision;
 
     }
 
