d8c7bea18b452165eb37ffe2ed99b66ea6a918e7
xvelastin <80051955+xvelastin@users.noreply.github.com>
2021-05-02 20:21:50 +0100

    added depth to text on left monitor

---
+++ JamulatorUnityProject/Assets/DepthToText.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DepthToText : MonoBehaviour
+{
+
+    [SerializeField] float currentDepth;
+    Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentDepth = Mathf.RoundToInt(SubmarineState.Instance.submarine.transform.position.y);
+        text.text = " Depth: \n" + currentDepth + "m";
+    }
+
+
+}
