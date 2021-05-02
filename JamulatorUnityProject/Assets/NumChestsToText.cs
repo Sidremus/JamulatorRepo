fcf90e37f362f9015108b4085b6497f097de3981
xvelastin <80051955+xvelastin@users.noreply.github.com>
2021-05-02 20:59:06 +0100

    began numhcests

---
+++ JamulatorUnityProject/Assets/NumChestsToText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumChestsToText : MonoBehaviour
+{
+    GameObject[] treasureChests;
+
+    private void Start()
+    {
+        
+    }
+
+
+
+}
